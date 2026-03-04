34227e2915eb5dbbd3b446efb14fea9bb4b768e0
Talles <worktalles@hotmail.com>
2026-03-03 23:29:58 -0300

    chore(prisma): adjust prisma client adapter usage

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
+import { PrismaClient } from './prisma-client'
 
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
