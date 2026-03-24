abde6914172a1f4c018d88ce3a33b5a09dc7a343
Talles <worktalles@hotmail.com>
2026-03-24 19:27:47 -0300

    chore(api): register routes in module index

--- api/src/http/routes/modules/index.ts
+++ api/src/http/routes/modules/index.ts
@@ -5,10 +5,13 @@ import { getModule } from './get.js'
 import { listModules } from './list.js'
 import { updateModule } from './update.js'
 
+import { generateModule } from './generate.js'
+
 export async function modulesIndex(app: FastifyInstance) {
   app.register(listModules)
   app.register(getModule)
   app.register(createModule)
   app.register(updateModule)
   app.register(deleteModule)
+  app.register(generateModule)
 }
